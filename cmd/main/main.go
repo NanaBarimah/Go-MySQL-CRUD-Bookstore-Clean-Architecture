1fca71fb3498965d2af46a01ecd4ff9fd96e00e5
NanaBarimah <barimahappiah@gmail.com>
2022-07-17 05:04:54 +0900

    Go-mux-CRUD-API-clean-architecture

---
+++ cmd/main/main.go
@@ -0,0 +1,41 @@
+// Flow of the project
+// Step 1: Started with the routes/bookstore-routes.go to create the routes to connect to the controllers
+// Step 2: Moved to the config/app.go to create the varible db and to return the variable db which will help the other files to interact with the db
+// Step 3: Next is the utils/utils.go to unmarshal the data for the request
+// Step 4: Create the models/book.go to give gives us a structure to help store something in the database
+// Step 5: Create the cmd/main/main.go to create the server which will define the localhost and show where the routers reside
+
+// routes/bookstore-routes.go required controllers package
+// config/app.go required GORM package
+// models/book.go required config package
+
+// The control are first in the ROUTES
+// ROUTES gives controls to the controllers
+// And Controllers give control to the models
+// In this sense the users interact with the routes and the routes send
+// control to the controllers where we have all the logic
+// Controllers have to perform some operations with the database
+// The operations of the database reside inside the models which book.go in this case
+// Hence the functions for the controllers are defined in the models
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/NanaBarimah/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+
+	fmt.Printf("Starting server...\n")
+	// ListenAndServe helps create the server
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
